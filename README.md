556c2e10e2b4b5897842a144cea653f94e881f27
-l <dbonineiv@gmail.com>
2022-05-30 17:49:25 -0700

    Added screenshot to images folder. Completed Readme.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ILW5Schedule
-Work Scheduler: Week 5 project
+# Simple daily scheduler
+
+**What it does**
+
+- This application allows you to save hourly reminders to help you keep track of your busy schedule! The current day and time are displayed on the top of the page. The main body of the application is comprised of time blocks for each hour of a standard work day. The current hour of the day is displayed in light blue (#8bd3dd), while past hours are displayed in a brighter color and future time blocks appear darker.
+
+**Step-by-Step instructions**
+
+- To use is application, simply click on a time block and type in whatever reminder you need. Then, click the "Save" button on the right side of that time block to save the reminder to your local storage. Once saved, the reminders will stay where you put them, even if you close the webpage.
+
+**A screenshot of the application**
+![Screenshot](./assets/images/screenshot.png)
+
+**Links**
+Website: https://love-ivy.github.io/ILW5Schedule/
+GitHub Repository: https://github.com/Love-Ivy/ILW5Schedule
+
+**Technologies Used**
+
+- html
+- css
+- javascript
+- Font Awesome
+- Google Fonts
+- moment.js
+
+**Author**
+Created by Ivy Lovegood
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,4 @@
-// ## User Story
-
-// ```md
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-// ```
-
-// ## Acceptance Criteria
-
-// ```md
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-var userschedule = [
-  {
-    id: "0",
-    reminder: "",
-  },
-  {
-    id: "1",
-    reminder: "",
-  },
-  {
-    id: "2",
-    reminder: "",
-  },
-  {
-    id: "3",
-    reminder: "",
-  },
-  {
-    id: "4",
-    reminder: "",
-  },
-  {
-    id: "5",
-    reminder: "",
-  },
-  {
-    id: "6",
-    reminder: "",
-  },
-  {
-    id: "7",
-    reminder: "",
-  },
-  {
-    id: "8",
-    reminder: "",
-  },
-];
-
 //Create display of current time 'moment.js'
-
 var day = moment().format("dddd");
 var date = moment().format("MMM Do YYYY");
 var hour = moment().format("h");
@@ -93,11 +28,8 @@ function loadTask() {
   for (let i = 0; i < timeblocks.length; i++) {
     let timeblock = timeblocks[i];
     let usertb = timeblock.children[1];
-    // let usertext = usertb.value;
     let utjson = localStorage.getItem(usertb.id);
     let userparse = JSON.parse(utjson);
-    console.log(usertb.id);
-    console.log("---------------");
     if (userparse !== null) {
       usertb.value = userparse;
     }
@@ -116,14 +48,6 @@ function saveTask(event) {
   let usertext = usertb.value;
   let utjson = JSON.stringify(usertext);
   localStorage.setItem(usertb.id, utjson);
-  console.log(usertb.id);
-  console.log(usertext);
 }
-
-// // Clear localstorage
-// function clearStorage() {
-//   localStorage.clear();
-// }
-// clearStorage();
 
 loadTask();
